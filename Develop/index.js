16bca749b87e3b6d368a4f8e14633a5ff8a6a644
jheersink8 <150620321+jheersink8@users.noreply.github.com>
2024-02-23 21:23:32 -0700

    Added questions to index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,18 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "TITLE: What is the title name of your project?", 
+    "DESCRIPTION: Provide a short description explaining the what, why, and how of your project: ", 
+    "INSTALLATION: What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running: ", 
+    "USAGE: Provide instructions and examples for usage: ",
+    "LICENSE: LICENSE STUFF HERE",
+    "CONTRIBUTING: If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so: ",
+    "TESTS: Go the extra mile and write tests for your application. Then provide examples on how to run them here: ",
+    "QUESTIONS: What is your GitHub username: ",
+    "QUESTIONS: What is your email address: ",
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
