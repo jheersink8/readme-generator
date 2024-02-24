bb371a50f7cb4f9920049ea52cf9f1aa0f986c00
jheersink8 <150620321+jheersink8@users.noreply.github.com>
2024-02-23 21:53:30 -0700

    exporting results to .md file. No formating is set yet.

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,12 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [
-    "TITLE: What is the title name of your project?", 
-    "DESCRIPTION: Provide a short description explaining the what, why, and how of your project: ", 
-    "INSTALLATION: What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running: ", 
+    "TITLE: What is the title name of your project?",
+    "DESCRIPTION: Provide a short description explaining the what, why, and how of your project: ",
+    "INSTALLATION: What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running: ",
     "USAGE: Provide instructions and examples for usage: ",
     "LICENSE: LICENSE STUFF HERE",
     "CONTRIBUTING: If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so: ",
@@ -15,10 +16,66 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log('Success!'))
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: questions[0],
+                name: 'title',
+            },
+            {
+                type: 'input',
+                message: questions[1],
+                name: 'description',
+            },
+            {
+                type: 'input',
+                message: questions[2],
+                name: 'installation',
+            },
+            {
+                type: 'input',
+                message: questions[3],
+                name: 'usage',
+            },
+            {
+                type: 'input',
+                message: questions[4],
+                name: 'license',
+            },
+            {
+                type: 'input',
+                message: questions[5],
+                name: 'contributing',
+            },
+            {
+                type: 'input',
+                message: questions[6],
+                name: 'tests',
+            },
+            {
+                type: 'input',
+                message: questions[7],
+                name: 'github',
+            },
+            {
+                type: 'input',
+                message: questions[8],
+                name: 'email',
+            },
+        ])
+        .then((results) => {
+            console.log(results);
+            writeToFile("README.md", JSON.stringify(results));
+        })
+};
 
 // Function call to initialize app
 init();
